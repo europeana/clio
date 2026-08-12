00c58773a75c71c1e8afe53d710249fef2b7199e
Simon Tzanakis <simon.tzanakis@europeana.eu>
2026-08-12 14:54:19 +0200

    MET-7231 Fix earlier bug of direct casting to Instant of Date objects returned from Solr client

--- clio-link-checking/clio-link-checking-service/src/main/java/eu/europeana/clio/link/checking/service/dao/SolrDao.java
+++ clio-link-checking/clio-link-checking-service/src/main/java/eu/europeana/clio/link/checking/service/dao/SolrDao.java
@@ -6,6 +6,7 @@ import eu.europeana.clio.link.checking.service.model.SampleRecord;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Collection;
+import java.util.Date;
 import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.List;
@@ -15,10 +16,10 @@ import java.util.Optional;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.request.SolrQuery;
 import org.apache.solr.client.solrj.request.SolrQuery.ORDER;
 import org.apache.solr.client.solrj.request.SolrQuery.SortClause;
-import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
@@ -28,120 +29,121 @@ import org.apache.solr.common.SolrDocumentList;
 @Slf4j
 public class SolrDao {
 
-    private static final String CONTENT_TIER_FIELD = "contentTier";
-    private static final String EDM_DATASET_NAME_FIELD = "edm_datasetName";
-    private static final String EDM_TYPE_FIELD = "proxy_edm_type";
-    private static final String IS_SHOWN_AT_FIELD = "provider_aggregation_edm_isShownAt";
-    private static final String IS_SHOWN_BY_FIELD = "provider_aggregation_edm_isShownBy";
-    private static final String METADATA_TIER_FIELD = "metadataTier";
-    private static final String RECORD_ID_FIELD = "europeana_id";
-    private static final String TIMESTAMP_UPDATE_FIELD = "timestamp_update";
+  private static final String CONTENT_TIER_FIELD = "contentTier";
+  private static final String EDM_DATASET_NAME_FIELD = "edm_datasetName";
+  private static final String EDM_TYPE_FIELD = "proxy_edm_type";
+  private static final String IS_SHOWN_AT_FIELD = "provider_aggregation_edm_isShownAt";
+  private static final String IS_SHOWN_BY_FIELD = "provider_aggregation_edm_isShownBy";
+  private static final String METADATA_TIER_FIELD = "metadataTier";
+  private static final String RECORD_ID_FIELD = "europeana_id";
+  private static final String TIMESTAMP_UPDATE_FIELD = "timestamp_update";
 
-    private final SolrClient solrClient;
+  private final SolrClient solrClient;
 
-    /**
-     * Constructor.
-     *
-     * @param solrClient The Solr client.
-     */
-    public SolrDao(SolrClient solrClient) {
-        this.solrClient = solrClient;
+  /**
+   * Constructor.
+   *
+   * @param solrClient The Solr client.
+   */
+  public SolrDao(SolrClient solrClient) {
+    this.solrClient = solrClient;
+  }
+
+  /**
+   * Get random sample records for a given dataset that have at least one link.
+   *
+   * @param datasetId The (Metis) dataset ID of the dataset for which to get random records.
+   * @param numberOfSampleRecords The number of random records needed.
+   * @return The list containing the required number of random records (or fewer if the dataset doesn't have that many).
+   * @throws PersistenceException In case of problems obtaining the sample records.
+   */
+  public List<SampleRecord> getRandomSampleRecords(String datasetId, int numberOfSampleRecords)
+      throws PersistenceException {
+
+    // Create query
+    final SolrQuery solrQuery = new SolrQuery("*:*");
+    solrQuery.setFilterQueries(EDM_DATASET_NAME_FIELD + ":" + datasetId + "_*",
+        IS_SHOWN_AT_FIELD + ":[* TO *] OR " + IS_SHOWN_BY_FIELD + ":[* TO *]");
+    solrQuery.setSort(new SortClause("random_" + System.currentTimeMillis(), ORDER.asc));
+    solrQuery.setStart(0);
+    solrQuery.setRows(numberOfSampleRecords);
+    solrQuery.setFields(CONTENT_TIER_FIELD, EDM_TYPE_FIELD, IS_SHOWN_AT_FIELD, IS_SHOWN_BY_FIELD,
+        METADATA_TIER_FIELD, RECORD_ID_FIELD, TIMESTAMP_UPDATE_FIELD);
+
+    // Get and return result.
+    return executeQuery(solrQuery).stream().map(SolrDao::convert).toList();
+  }
+
+  private static SampleRecord convert(SolrDocument result) {
+
+    // Get the isShownAt links.
+    final Map<LinkType, Set<String>> links = new EnumMap<>(LinkType.class);
+    final List<?> isShownAtLinks = (List<?>) result.getFieldValue(IS_SHOWN_AT_FIELD);
+    if (isShownAtLinks != null) {
+      for (Object link : isShownAtLinks) {
+        links.computeIfAbsent(LinkType.IS_SHOWN_AT, key -> new HashSet<>()).add(link.toString());
+      }
     }
 
-    /**
-     * Get random sample records for a given dataset that have at least one link.
-     *
-     * @param datasetId             The (Metis) dataset ID of the dataset for which to get random records.
-     * @param numberOfSampleRecords The number of random records needed.
-     * @return The list containing the required number of random records (or fewer if the dataset
-     * doesn't have that many).
-     * @throws PersistenceException In case of problems obtaining the sample records.
-     */
-    public List<SampleRecord> getRandomSampleRecords(String datasetId, int numberOfSampleRecords)
-            throws PersistenceException {
-
-        // Create query
-        final SolrQuery solrQuery = new SolrQuery("*:*");
-        solrQuery.setFilterQueries(EDM_DATASET_NAME_FIELD + ":" + datasetId + "_*",
-                IS_SHOWN_AT_FIELD + ":[* TO *] OR " + IS_SHOWN_BY_FIELD + ":[* TO *]");
-        solrQuery.setSort(new SortClause("random_" + System.currentTimeMillis(), ORDER.asc));
-        solrQuery.setStart(0);
-        solrQuery.setRows(numberOfSampleRecords);
-        solrQuery.setFields(CONTENT_TIER_FIELD, EDM_TYPE_FIELD, IS_SHOWN_AT_FIELD, IS_SHOWN_BY_FIELD,
-                METADATA_TIER_FIELD, RECORD_ID_FIELD, TIMESTAMP_UPDATE_FIELD);
-
-        // Get and return result.
-        return executeQuery(solrQuery).stream().map(SolrDao::convert).toList();
+    // Get the isShownBy links.
+    final List<?> isShownByLinks = (List<?>) result.getFieldValue(IS_SHOWN_BY_FIELD);
+    if (isShownByLinks != null) {
+      for (Object link : isShownByLinks) {
+        links.computeIfAbsent(LinkType.IS_SHOWN_BY, key -> new HashSet<>()).add(link.toString());
+      }
     }
 
-    private static SampleRecord convert(SolrDocument result) {
+    // Get the last indexed time (update time). This should really exist.
+    final Instant lastIndexedTime = toInstant(result.getFieldValue(TIMESTAMP_UPDATE_FIELD));
 
-        // Get the isShownAt links.
-        final Map<LinkType, Set<String>> links = new EnumMap<>(LinkType.class);
-        final List<?> isShownAtLinks = (List<?>) result.getFieldValue(IS_SHOWN_AT_FIELD);
-        if (isShownAtLinks != null) {
-            for (Object link : isShownAtLinks) {
-                links.computeIfAbsent(LinkType.IS_SHOWN_AT, key -> new HashSet<>()).add(link.toString());
-            }
-        }
-
-        // Get the isShownBy links.
-        final List<?> isShownByLinks = (List<?>) result.getFieldValue(IS_SHOWN_BY_FIELD);
-        if (isShownByLinks != null) {
-            for (Object link : isShownByLinks) {
-                links.computeIfAbsent(LinkType.IS_SHOWN_BY, key -> new HashSet<>()).add(link.toString());
-            }
-        }
-
-        // Get the last indexed time (update time). This should really exist.
-        final Instant lastIndexedTime = Optional
-                .ofNullable(result.getFieldValue(TIMESTAMP_UPDATE_FIELD)).map(Instant.class::cast)
-                .orElse(Instant.EPOCH);
-
-        // Get the edm:type.
-        final List<String> edmTypes = Optional
-                .ofNullable((List<?>) result.getFieldValue(EDM_TYPE_FIELD)).stream()
-                .flatMap(Collection::stream).filter(Objects::nonNull).map(String.class::cast)
-                .filter(type -> !type.isBlank()).distinct().toList();
-        final String recordId = (String) result.getFieldValue(RECORD_ID_FIELD);
-        if (edmTypes.size() > 1 && log.isInfoEnabled()) {
-            log.info("Found multiple types for record '{}': {}", recordId,
-                    String.join(", ", edmTypes));
-        }
-        final String edmType = edmTypes.isEmpty() ? null : edmTypes.getFirst();
-
-        // Done.
-        return new SampleRecord(recordId, lastIndexedTime, edmType,
-                (String) result.getFieldValue(CONTENT_TIER_FIELD),
-                (String) result.getFieldValue(METADATA_TIER_FIELD), links);
+    // Get the edm:type.
+    final List<String> edmTypes = Optional
+        .ofNullable((List<?>) result.getFieldValue(EDM_TYPE_FIELD)).stream()
+        .flatMap(Collection::stream).filter(Objects::nonNull).map(String.class::cast)
+        .filter(type -> !type.isBlank()).distinct().toList();
+    final String recordId = (String) result.getFieldValue(RECORD_ID_FIELD);
+    if (edmTypes.size() > 1 && log.isInfoEnabled()) {
+      log.info("Found multiple types for record '{}': {}", recordId,
+          String.join(", ", edmTypes));
     }
+    final String edmType = edmTypes.isEmpty() ? null : edmTypes.getFirst();
 
-    /**
-     * Determines the last time that there was an update (i.e. a record was indexed). We do this by
-     * returning the maximum of all update timestamps.
-     *
-     * @return The last update instant.
-     */
-    public Instant getLastUpdateTime() throws PersistenceException {
+    // Done.
+    return new SampleRecord(recordId, lastIndexedTime, edmType,
+        (String) result.getFieldValue(CONTENT_TIER_FIELD),
+        (String) result.getFieldValue(METADATA_TIER_FIELD), links);
+  }
 
-        // Create query
-        final SolrQuery solrQuery = new SolrQuery("*:*");
-        solrQuery.setSort(new SortClause(TIMESTAMP_UPDATE_FIELD, ORDER.desc));
-        solrQuery.setStart(0);
-        solrQuery.setRows(1);
-        solrQuery.setFields(TIMESTAMP_UPDATE_FIELD);
+  /**
+   * Determines the last time that there was an update (i.e. a record was indexed). We do this by returning the maximum of all
+   * update timestamps.
+   *
+   * @return The last update instant.
+   */
+  public Instant getLastUpdateTime() throws PersistenceException {
 
-        // Get and return result
-        return executeQuery(solrQuery).stream().findFirst()
-                .map(document -> document.getFieldValue(TIMESTAMP_UPDATE_FIELD)).map(Instant.class::cast)
-                .orElse(Instant.EPOCH);
+    // Create query
+    final SolrQuery solrQuery = new SolrQuery("*:*");
+    solrQuery.setSort(new SortClause(TIMESTAMP_UPDATE_FIELD, ORDER.desc));
+    solrQuery.setStart(0);
+    solrQuery.setRows(1);
+    solrQuery.setFields(TIMESTAMP_UPDATE_FIELD);
+
+    // Get and return result
+    return executeQuery(solrQuery).stream().findFirst()
+                                  .map(document -> toInstant(document.getFieldValue(TIMESTAMP_UPDATE_FIELD)))
+                                  .orElse(Instant.EPOCH);
+  }
+
+  private SolrDocumentList executeQuery(SolrQuery solrQuery) throws PersistenceException {
+    try {
+      return this.solrClient.query(solrQuery).getResults();
+    } catch (SolrServerException | IOException e) {
+      throw new PersistenceException("Problem occurred while obtaining the last update time.", e);
     }
+  }
 
-    private SolrDocumentList executeQuery(SolrQuery solrQuery) throws PersistenceException {
-        try {
-            return this.solrClient.query(solrQuery).getResults();
-        } catch (SolrServerException | IOException e) {
-            throw new PersistenceException("Problem occurred while obtaining the last update time.", e);
-        }
-    }
+  private static Instant toInstant(Object value) {
+    return value instanceof Date date ? date.toInstant() : Instant.EPOCH;
+  }
 }
